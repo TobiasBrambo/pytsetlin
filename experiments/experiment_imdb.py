7bb2f1d3567cde67072d40e86c73ca4cb33ea8ab
Sebastianostby <sebastiaos@uia.no>
2025-01-08 15:43:35 +0100

    add literal budget, fix bug

--- experiments/experiment_imdb.py
+++ experiments/experiment_imdb.py
@@ -11,17 +11,18 @@ from tsetlin_machine import TsetlinMachine
 
 if __name__ == "__main__":
 
-    x_train, y_train, x_test, y_test = get_imdb(save_vocab=True)
+    x_train, y_train, x_test, y_test = get_imdb()
 
     tm = TsetlinMachine(n_clauses=100,
-                        threshold=50,
-                        s=2.0)
+                        threshold=500,
+                        s=10.0,
+			n_literal_budget=5)
 
     tm.set_train_data(x_train, y_train)
 
     tm.set_eval_data(x_test, y_test)
 
-    r = tm.train(training_epochs=10)
+    r = tm.train(training_epochs=100)
     
     print(r)
 
